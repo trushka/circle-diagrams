c5af0c2a52aa8969d3c4032a2c6b952a90f88dd9
trushka <trueshko@gmail.com>
2021-10-19 18:20:12 +0300

    add titles

--- pie/pie.js
+++ pie/pie.js
@@ -1,5 +1,5 @@
 function createPIE() {
-	const PIE_SELECTOR = '.diagram',
+	const PIE_SELECTOR = '.pie-vidget',
 		ITEM_SELECTOR = '.data-list>div', // selectors below must be inside this element
 		VALUE_SELECTOR = 'dt', //background-color and content from this elements uses for diagram
 		DESCRIPTION_SELECTOR = 'dd'
@@ -12,21 +12,23 @@ function createPIE() {
 	let sum=0, start0=0;
 	$('circle', svg).remove();
 	$(ITEM_SELECTOR).each((i,el)=>{
-		let val=parseFloat($(VALUE_SELECTOR, el).text())/100;
-		sum+=val;
+		let valText=$(VALUE_SELECTOR, el).html();
+		 val=parseFloat(valText)/100;
+		 sum+=val;
 		sectors.push({
-			val: val,
+			val, valText, el,
 			text: $(DESCRIPTION_SELECTOR, el).html(),
-			el: el,
 			color: $(VALUE_SELECTOR, el).css('background-color')
 		})
 	});
 	sectors.sort((a,b)=>a.val-b.val)
 	.forEach((item,i)=>{
 		item.val/=sum;
-		const {val, text, el, color} = item, start=start0, isLast = i+1==sectors.length;
-		const title=$('<div class="pie-title">').hide().html(text).appendTo(container);
-		const sector=$('circle', '<svg><circle pathLength="1">').css({
+		const {val, valText, text, el, color} = item,
+		 start=start0,
+		 isLast = i+1==sectors.length,
+		 title=$('<div class="pie-title">').html(`<div>${valText}</div><div>${text}</div>`).appendTo(container),
+		 sector=$('circle', '<svg><circle pathLength="1">').css({
 			stroke: color,
 			'stroke-dashoffset': -start,
 			'stroke-dasharray': val+.0015+', '+(1-val-.001)
@@ -34,18 +36,38 @@ function createPIE() {
 		sector.add(el).mouseenter(hover)
 		function hover() {
 			let turn=angle.value/360;
-			const begin=start+turn,
-			 end=(+isLast||start+val)+turn,
+			const begin=start+turn-.0002,
+			 end=(+isLast||start+val)+turn-.0005,
 			 turn0=parseFloat(pointer.css('--turn'));
-			turn=isLast?end:distTo(begin, .5)<distTo(end, .5)?begin:end;
+
+			turn=isLast?end:distTo(begin, .5)<distTo(end, .5)?begin:end-.0008;
+
+			const absTurn = ((turn+.25)%1+1)%1,
+			 left = absTurn>.5,
+			 up = Math.abs(absTurn)>.124 && Math.abs(turn-(left?begin:end))<.002 || Math.abs(absTurn-.5)<.125;
+			//console.log( absTurn, up)
+
 			turn=((turn-turn0+.5)%1+1)%1-.5;
 			pointer.css({
 				'--turn': turn0+turn+'turn',
+				fill: color,
 				transition: Math.max(Math.abs(turn*1.5), .2)+'s'
+			}).off('transitionend').on('transitionend', e=>{
+				const rect=title[0].getBoundingClientRect(),
+				 svgRect=svg[0].getBoundingClientRect(),
+				 divRect=container[0].getBoundingClientRect(),
+				 srcRect=pointer[0].querySelector('.pie-marker').getBoundingClientRect(),
+				 side=left?'left':'right',
+				 x=srcRect[side],
+				 y=srcRect[up?'bottom':'top'];
+				title.css({
+					transform: `translate(${x}px, ${y}px) translate(${-left*100}%, ${-up*100}%)`
+				}).addClass('visible')[left?'addClass':'removeClass']('pie-left')
 			});
+			$('.pie-title.visible').not(title[0]).removeClass('visible');
 		}
 		start0+=val;
-		if (isLast) document.readyState=='complete'?hover():$(document).one('load', hover);
+		if (isLast) document.readyState=='complete'?hover():$(window).one('load', hover);
 	})
 }
 function distTo(a, base=1){
